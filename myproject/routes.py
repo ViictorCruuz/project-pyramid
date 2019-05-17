f61fa3f917861f5b6fa43cf0842ce24dd9ad4fac
Victor Cruz <victor.cruz@geru.com.br>
2019-05-17 17:18:09 -0300

    Creating a route and view, and creates templates for web page

--- myproject/routes.py
+++ myproject/routes.py
@@ -1,3 +1,5 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('victor', '/victor')
+    config.add_route('outra', '/victor/outra')
--- myproject/views/default.py
+++ myproject/views/default.py
@@ -4,3 +4,13 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
     return {'project': 'myproject'}
+
+
+@view_config(route_name='victor', renderer='../templates/victor.jinja2')
+def victor(request):
+    return {'project': 'myproject'}
+
+
+@view_config(route_name='outra', renderer='../templates/outra.jinja2')
+def outra(request):
+    return {'project': 'myproject'}
